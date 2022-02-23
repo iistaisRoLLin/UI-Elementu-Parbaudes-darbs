ba087a484771fbf3bc667a48d54d03beeaf2407b
Kristers <kristers.cernisevs@gmail.com>
2022-02-23 14:09:58 +0200

    Pievienota izvele izveleties apģērbu un pievienots neliels apraksts par supervaroņiem

---
+++ Assets/Skripti/ParaditAttelu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParaditAttelu : MonoBehaviour {
+	public GameObject cepure;
+	public GameObject pufaika;
+	public GameObject cimdi;
+	public GameObject bikses;
+	public Sprite[] atteluMasivs; 
+	public GameObject attelaTuretajs;
+	public void cepureAttelosana(bool vertiba){
+		cepure.SetActive(vertiba);
+	}
+	public void pufaikaAttelosana(bool vertiba){
+		pufaika.SetActive(vertiba);
+	}
+	public void cimdiAttelosana(bool vertiba){
+		cimdi.SetActive(vertiba);
+	}
+	public void biksesAttelosana(bool vertiba){
+		bikses.SetActive(vertiba);
+	}
+	public void izkritosais(int skaitlis){
+		if (skaitlis == 0){
+			attelaTuretajs.GetComponent<Image>().sprite = atteluMasivs [0];
+
+		}else if(skaitlis == 1){
+			attelaTuretajs.GetComponent<Image>().sprite = atteluMasivs [1];
+		}
+}
+}
