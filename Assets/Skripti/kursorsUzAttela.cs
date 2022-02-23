fba317053e50f63af1c1a5ef8a36fba9f449b0d1
Kristers <kristers.cernisevs@gmail.com>
2022-02-23 15:26:20 +0200

    Pievienots audio slaiderim un pogai

---
+++ Assets/Skripti/kursorsUzAttela.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kursorsUzAttela : MonoBehaviour {
+
+	public AudioSource skanasAvots;
+	public AudioClip skanaKoAtskanot;
+	public void uzbiditsUzAttela(){
+		skanasAvots.PlayOneShot (skanaKoAtskanot);
+	}
+	public void kursorsNoietNoAttela(){
+		skanasAvots.Stop ();
+	}
+}
